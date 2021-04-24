983f676a9d0f943c40252e0c0b9740fc35a9b204
Paouky <Paouky@users.noreply.github.com>
2021-04-24 23:49:29 +0300

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+      backgroundColor: theme => ({
+          'almostBlack': '#0a0a0a',
+      })
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
